7b52995cee6150698cb9eaaf05e4b94f3b64da65
fabanc <fabien.ancelin@gmail.com>
2015-11-19 22:50:16 -0500

    Added more input and input data structure to the C# wrapper.
    Added the concept of cell in the C# wrapping library.
    
    Added the following attributes to the C# Voronoi Wrapper:
    1. Input Points.
    2. Input Segments.
    3. Output Vertices.
    4. Output Edges.
    5. Output Cells.

---
+++ SharpBoostVoronoi/SharpBoostVoronoi/BoostVoronoi.cs
@@ -0,0 +1,40 @@
+﻿using SharpBoostVoronoi.Input;
+using SharpBoostVoronoi.Output;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpBoostVoronoi
+{
+    public class BoostVoronoi
+    {
+
+        /// <summary>
+        /// The input points used to construct the voronoi diagram
+        /// </summary>
+        public Point InputPoints { get; private set; }
+
+        /// <summary>
+        /// The input segments used to construct the voronoi diagram
+        /// </summary>
+        public Point InputSegments { get; private set; }
+
+        /// <summary>
+        /// The output list of vertices
+        /// </summary>
+        public List<Vertex> Vertices { get; set; }
+
+        /// <summary>
+        /// The output list of edges
+        /// </summary>
+        public List<Edge> Edges { get; set; }
+
+        /// <summary>
+        /// The output list of cells
+        /// </summary>
+        public List<Cell> Cells { get; set; }
+
+    }
+}
---
+++ SharpBoostVoronoi/SharpBoostVoronoi/Outputs/Cell.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpBoostVoronoi.Output
+{
+    public class Cell
+    {
+        /// <summary>
+        /// The voronoi cell identifier
+        /// </summary>
+        public long Index { get; set; }
+
+        /// <summary>
+        /// The index of the source feature
+        /// </summary>
+        public long Site { get; set; }
+
+        /// <summary>
+        /// True if the cell is made from a point
+        /// </summary>
+        public bool ContainsPoint { get; set; }
+
+        /// <summary>
+        /// True if the cell is made from a segment
+        /// </summary>
+        public bool ContainsSegment { get; set; }
+
+        /// <summary>
+        /// Indexes of the segment that makes the cell
+        /// </summary>
+        public List<long> Segments { get; set; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="t">A tuple returned by the CLR wrapper.</param>
+        public Cell(Tuple <long, long, bool, bool, List<long>> t)
+        {
+            Index = t.Item1;
+            Site = t.Item2;
+            ContainsPoint = t.Item3;
+            ContainsSegment = t.Item4;
+            Segments = t.Item5;
+        }
+    }
+}
