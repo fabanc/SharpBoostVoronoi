6a09ef2273bd231a8b3d043301bbce90d5647919
fabanc <fabien.ancelin@gmail.com>
2016-06-07 07:50:33 -0400

    Added a performance test project.
    Added a performance test project. This allow me to benchmark the tool
    and test its capacity on different plate-forms. I also modified the
    SharpBoostVoronoi project to disable the "Just my code" debugging
    option.

---
+++ SharpBoostVoronoi/PerformanceTesting/Program.cs
@@ -0,0 +1,87 @@
+﻿using SharpBoostVoronoi;
+using SharpBoostVoronoi.Input;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerformanceTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Point> points = new List<Point>();
+            List<Segment> segments = new List<Segment>();
+            int limit = 10;
+            segments = PopulateSegment(limit, limit);
+            ConstructAndMeasure(ref points, ref segments);
+
+
+            points = new List<Point>();
+            segments = PopulateSegment(100, 100);
+            ConstructAndMeasure(ref points, ref segments);
+
+            points = new List<Point>();
+            segments = PopulateSegment(100, 1000);
+            ConstructAndMeasure(ref points, ref segments);
+
+            points = new List<Point>();
+            segments = PopulateSegment(100, 2500);
+            ConstructAndMeasure(ref points, ref segments);
+
+
+            points = new List<Point>();
+            segments = PopulateSegment(100, 5000);
+            ConstructAndMeasure(ref points, ref segments);
+
+            points = new List<Point>();
+            segments = PopulateSegment(100, 7500);
+            ConstructAndMeasure(ref points, ref segments);
+
+            points = new List<Point>();
+            segments = PopulateSegment(100, 10000);
+            ConstructAndMeasure(ref points, ref segments);
+
+            Console.In.ReadLine();
+        }
+
+
+        static List<Segment> PopulateSegment(int maxX, int maxY)
+        {
+            List<Segment>  segments = new List<Segment>();
+            for (int i = 0; i < maxX; i++)
+            {
+                for (int j = 0; j < maxY; j++)
+                {
+                    segments.Add(new Segment(new Point(i, j), new Point(i + 1, j + 1)));
+                }
+            }
+            return segments;
+        }
+
+        static void ConstructAndMeasure(ref List<Point> inputPoints, ref List<Segment> inputSegments)
+        {
+            Console.WriteLine(String.Format("Testing with {0} points and {1} segments", inputPoints.Count, inputSegments.Count));
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            BoostVoronoi bv = new BoostVoronoi();
+            foreach (var point in inputPoints)
+                bv.AddPoint(point.X, point.Y);
+
+            foreach (var segment in inputSegments)
+                bv.AddSegment(segment.Start.X, segment.Start.Y, segment.End.X, segment.End.Y);
+
+
+
+            bv.Construct();
+
+            // Stop timing.
+            stopwatch.Stop();
+            Console.WriteLine("Time elapsed: {0:hh\\:mm\\:ss\\:ff}", stopwatch.Elapsed);
+
+        }
+    }
+}
